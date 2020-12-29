b7d6b2b3cd7df9a33bb32e6085e49cd2b15cd516
covertjustice <sardarbavneetsingh@gmail.com>
2020-12-29 14:27:12 +0530

    Django part 2 commit

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse('Hello, World!!!')
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
