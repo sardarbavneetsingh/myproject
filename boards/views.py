0c339df4d55d7c4560c0bc1928a4e828effe2afc
covertjustice <sardarbavneetsingh@gmail.com>
2021-01-12 00:16:58 +0530

    URLs - Basic and Advanced

--- boards/views.py
+++ boards/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
