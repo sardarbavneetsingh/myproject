830e7391f0877020b9a910eee8786aa89962ed22
covertjustice <sardarbavneetsingh@gmail.com>
2021-02-02 03:32:44 +0530

    Part 3 implemented excluding "Creating Forms the Right Way"

--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from django.contrib.auth.models import User
+from .models import Board, Topic, Post
 
 # Create your views here.
-from django.http import HttpResponse, Http404
-from .models import Board
 
 def home(request):
     boards = Board.objects.all()
@@ -12,3 +13,27 @@ def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board': board})
 
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject = subject,
+            board = board,
+            starter = user
+        )
+
+        post = Post.objects.create(
+            message = message,
+            topic = topic,
+            created_by = user
+        )
+
+        return redirect('board_topics', pk=board.pk)
+    
+    return render(request, 'new_topic.html', {'board': board})
