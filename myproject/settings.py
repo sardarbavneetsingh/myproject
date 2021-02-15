1108f160cea32420d58a4d4ca16bd5943bab4bb5
covertjustice <sardarbavneetsingh@gmail.com>
2021-02-15 09:37:24 +0530

    Part 3 complete

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'boards'
-]
+    'widget_tweaks',
+    'boards',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
